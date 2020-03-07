3ca592451e1edadb9f6416085162e7cefa799a44
dealer426 <burnssamuel01@gmail.com>
2020-03-07 15:29:29 -0500

    yay for clickers

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,7 @@ const makeCookieClickerButton = (buttonClickElement, dashboardElement, cookieObj
   buttonClickElement.addEventListener('click', ()=>{
       cookieObject.cookieCounter();
       updateClicker(dashboardElement, cookieObject);
+      makeCookieCompanionClicker(cookieObject);
   })
 }
 
@@ -20,11 +21,24 @@ const resetClicker = (buttonClickElement, dashboardElement, cookieObject) => {
   })
 }
 
+const makeCookieCompanionClicker = (cookieObject) => {
+    if(cookieObject.getCookieCount() === 10)
+    {
+      alert('You reached 10 clicks!');
+      buttonCookieCompanionClicker.classList.remove('hide');
+      buttonCookieCompanionClicker.classList.add('show');
+      buttonCookieCollectiveCulminatioCompounder.classList.remove('hide');
+      buttonCookieCollectiveCulminatioCompounder.classList.add('show');
+    }
+}
+
 
 const myCookie = new Cookie();
 const dashboardElement = document.querySelector('.dashboard_cookiecounter');
 const buttonClickElement = document.querySelector('.cookie_clicker');
 const buttonResetElement = document.querySelector('.cookie_reset');
+const buttonCookieCompanionClicker = document.querySelector('.cookie_companion_clicker');
+const buttonCookieCollectiveCulminatioCompounder = document.querySelector('.cookie_collective_culmination_compounder');
 
 
 
