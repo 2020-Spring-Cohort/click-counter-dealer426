8e78e44a5864d2770cb94c4e1c7404ca1038cae2
dealer426 <burnssamuel01@gmail.com>
2020-03-09 10:01:29 -0400

    Final push

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -1,14 +1,17 @@
 class Cookie {
     constructor() {
         this._clicker = 98;
+        this._tempClicker = 98;
     }
 
     cookieCounter() {   
         this._clicker += 1;
+        this._tempClicker += 1;
     }
 
     cookieReset() { 
         this._clicker = 0;  
+    
     }
 
     getCookieCount() {   
@@ -18,5 +21,13 @@ class Cookie {
     addCompanion(amount) {
 
         this._clicker += amount;
+    }
+
+    cookieTempReset(){
+        this._tempClicker = 0;
+    }
+
+    getCookieTempCount(){
+        return this._tempclicker;
     }
 }
